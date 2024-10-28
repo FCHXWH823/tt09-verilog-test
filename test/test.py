3e57c57895862745506f99eac1009d51f36c8263
Weihua <fch@10-18-219-33.dynapool.wireless.nyu.edu>
2024-10-28 12:56:41 -0400

    koggestone8

--- test/test.py
+++ test/test.py
@@ -9,32 +9,20 @@ from cocotb.triggers import ClockCycles
 @cocotb.test()
 async def test_project(dut):
     dut._log.info("Start")
-
+    max_val = 0xFF
     # Set the clock period to 10 us (100 KHz)
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
+    for a in range(1<<8):
+        for b in range(1<<8):
+            dut.a.value = a
+            dut.b.value = b
+            await ClockCycles(dut.clk, 10)
 
-    # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+            expected_sum = (a + b) & max_val  # 4-bit sum
 
-    dut._log.info("Test project behavior")
+            # Check if the results match the expected values
+            assert dut.sum.value == expected_sum, f"Test failed with a={a}, b={b}: sum={dut.sum.value}, expected={expected_sum}"
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+            # Print the test result (optional)
+            print(f"PASS: a={a}, b={b} -> sum={dut.sum.value}")
